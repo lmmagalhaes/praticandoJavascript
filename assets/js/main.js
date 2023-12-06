7ed2e0943f0fc853c80f1991017dde3f6452f6c9
lmmagalhaes <leo.maga17@gmail.com>
2023-12-06 08:20:58 -0300

    Exercicio Calculadora

---
+++ assets/js/main.js
@@ -0,0 +1,73 @@
+function criaCalculadora() {
+  return {
+    display: document.querySelector('.display'),
+
+    inicia() {
+      this.cliqueBotoes()
+      this.pressionaEnter()
+    },
+
+    pressionaEnter() {
+      this.display.addEventListener('keyup', (e) => {
+        if (e.keyCode === 13) {
+          this.equal()
+        }
+      })
+    },
+
+    cliqueBotoes() {
+      document.addEventListener('click', (e) => {
+        const el = e.target
+
+        if (el.classList.contains('btn-num')) {
+          this.btnParaDisplay(el.innerText)
+        }
+
+        if (el.classList.contains('btn-clear')) {
+          this.limpaInput()
+        }
+
+        if (el.classList.contains('btn-apagaum')) {
+          this.apagaUm()
+        }
+
+        if (el.classList.contains('btn-eq')) {
+          this.equal()
+        }
+      })
+    },
+
+    btnParaDisplay(valor) {
+      this.display.value += valor
+    },
+
+    limpaInput() {
+      this.display.value = ''
+    },
+
+    apagaUm() {
+      this.display.value = this.display.value.slice(0, -1)
+    },
+
+    equal() {
+      let conta = this.display.value
+
+      try {
+        conta = eval(conta)
+
+        if (!conta) {
+          alert('Conta Inválida!')
+          return
+        }
+
+        this.display.value = String(conta)
+      } catch (error) {
+        alert('Conta Inválida')
+        return
+      }
+    },
+  }
+}
+
+const calculadora = criaCalculadora()
+calculadora.inicia()
